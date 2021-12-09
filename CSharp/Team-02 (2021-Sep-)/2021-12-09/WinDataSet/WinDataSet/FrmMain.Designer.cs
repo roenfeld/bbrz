38134c92db438d244d96ba2b12751d0ca26e70db
Walter Roenfeld <roenfeld@yahoo.com>
2021-12-09 08:14:00 +0100

    WR

---
+++ CSharp/Team-02 (2021-Sep-)/2021-12-09/WinDataSet/WinDataSet/FrmMain.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace WinDataSet
+{
+  partial class FrmMain
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // FrmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(591, 330);
+      this.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+      this.Name = "FrmMain";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
