65e34ae32415626e67aea3469f82fcfc31447eb7
Walter Roenfeld <roenfeld@yahoo.com>
2021-12-16 14:14:48 +0100

    WR

--- CSharp/Team-02 (2021-Sep-)/2021-12-16/ConAdo/ConAdo/Program.cs
+++ CSharp/Team-02 (2021-Sep-)/2021-12-16/ConAdo/ConAdo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,33 @@ namespace ConAdo
   {
     static void Main(string[] args)
     {
+      var cb = new SqlConnectionStringBuilder();
+      cb.DataSource = @"(local)\SQL2K17";
+      cb.InitialCatalog = "CodersBay";
+      cb.IntegratedSecurity = true;
+      
+      var con = new SqlConnection();
+      con.ConnectionString = cb.ConnectionString;
+      con.Open();
+
+      //var adapter = new SqlDataAdapter("SELECT * FROM employees", con);
+
+      var cmd = new SqlCommand();
+      cmd.CommandText = "SELECT * FROM employees";
+      cmd.Connection = con;
+
+      var adapter = new SqlDataAdapter();
+      adapter.SelectCommand = cmd;
+
+      var ds = new DataSet();
+      adapter.Fill(ds);
+
+      con.Close();
+    }
+
+
+    static void Ado_DataReader()
+    { 
       // Server=myServerName\myInstanceName;Database=myDataBase;User Id=myUsername;Password=myPassword;
       // Data Source = 190.190.200.100; Initial Catalog = myDataBase; User ID = myUsername; Password = myPassword;
       //  Trusted_Connection=True;  Integrated Security=SSPI
@@ -22,12 +50,13 @@ namespace ConAdo
 
       var cmd = new SqlCommand();
       cmd.Connection = con;
-      cmd.CommandText = "SELECT * FROM employees";
+      cmd.CommandText = "SELECT * FROM locations";
       var rdr = cmd.ExecuteReader();
-
+      
       while(rdr.Read())
       {
-        Console.WriteLine($"{rdr.GetValue(0)}:{rdr.GetValue(2)} {rdr.GetValue(1)}");
+        Console.WriteLine($"{rdr.GetInt32(0)}:{rdr.GetValue(2)} {rdr.GetValue(1)}");
+        Console.WriteLine($" {rdr.GetValue(4)}");
       }
 
       con.Close();
