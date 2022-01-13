8bbfba1a53c9f954feacf6fefad8ed311c11f6e3
Walter Roenfeld <roenfeld@yahoo.com>
2022-01-13 13:46:20 +0100

    WR

---
+++ CSharp/Team-02 (2021-Sep-)/UebungsPrj/WinEmployees/DAL_Test/BasicTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DAL_Test
+{
+  [TestClass]
+  public class BasicTests
+  {
+    [TestMethod]
+    public void Regions()
+    {
+      var ds = new DAL.DsCodersBay();
+
+      DAL.DsCodersBay.FillRegions(ds);
+    }
+
+  }
+}
---
+++ CSharp/Team-02 (2021-Sep-)/UebungsPrj/WinEmployees/WinEmployees/FrmMain.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WinEmployees
+{
+  partial class FrmMain
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+      this.navMain = new System.Windows.Forms.BindingNavigator(this.components);
+      this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+      this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+      this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+      this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+      this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+      this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+      this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+      this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+      this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+      this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+      this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+      this.bsRegions = new System.Windows.Forms.BindingSource(this.components);
+      this.dgvRegions = new System.Windows.Forms.DataGridView();
+      this.dsMain = new DAL.DsCodersBay();
+      this.regionidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.regionnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      ((System.ComponentModel.ISupportInitialize)(this.navMain)).BeginInit();
+      this.navMain.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.bsRegions)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dgvRegions)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dsMain)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // navMain
+      // 
+      this.navMain.AddNewItem = this.bindingNavigatorAddNewItem;
+      this.navMain.BindingSource = this.bsRegions;
+      this.navMain.CountItem = this.bindingNavigatorCountItem;
+      this.navMain.DeleteItem = this.bindingNavigatorDeleteItem;
+      this.navMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem});
+      this.navMain.Location = new System.Drawing.Point(0, 0);
+      this.navMain.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+      this.navMain.MoveLastItem = this.bindingNavigatorMoveLastItem;
+      this.navMain.MoveNextItem = this.bindingNavigatorMoveNextItem;
+      this.navMain.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+      this.navMain.Name = "navMain";
+      this.navMain.PositionItem = this.bindingNavigatorPositionItem;
+      this.navMain.Size = new System.Drawing.Size(784, 29);
+      this.navMain.TabIndex = 0;
+      this.navMain.Text = "bindingNavigator1";
+      // 
+      // bindingNavigatorMoveFirstItem
+      // 
+      this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+      this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+      this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+      this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 26);
+      this.bindingNavigatorMoveFirstItem.Text = "Erste verschieben";
+      // 
+      // bindingNavigatorMovePreviousItem
+      // 
+      this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+      this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+      this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+      this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 26);
+      this.bindingNavigatorMovePreviousItem.Text = "Vorherige verschieben";
+      // 
+      // bindingNavigatorSeparator
+      // 
+      this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+      this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 29);
+      // 
+      // bindingNavigatorPositionItem
+      // 
+      this.bindingNavigatorPositionItem.AccessibleName = "Position";
+      this.bindingNavigatorPositionItem.AutoSize = false;
+      this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 12F);
+      this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+      this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 29);
+      this.bindingNavigatorPositionItem.Text = "0";
+      this.bindingNavigatorPositionItem.ToolTipText = "Aktuelle Position";
+      // 
+      // bindingNavigatorCountItem
+      // 
+      this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+      this.bindingNavigatorCountItem.Size = new System.Drawing.Size(59, 26);
+      this.bindingNavigatorCountItem.Text = "von {0}";
+      this.bindingNavigatorCountItem.ToolTipText = "Die Gesamtanzahl der Elemente.";
+      // 
+      // bindingNavigatorSeparator1
+      // 
+      this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+      this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 29);
+      // 
+      // bindingNavigatorMoveNextItem
+      // 
+      this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+      this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+      this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+      this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 26);
+      this.bindingNavigatorMoveNextItem.Text = "Nächste verschieben";
+      // 
+      // bindingNavigatorMoveLastItem
+      // 
+      this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+      this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+      this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+      this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 26);
+      this.bindingNavigatorMoveLastItem.Text = "Letzte verschieben";
+      // 
+      // bindingNavigatorSeparator2
+      // 
+      this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+      this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 29);
+      // 
+      // bindingNavigatorAddNewItem
+      // 
+      this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+      this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+      this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+      this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 26);
+      this.bindingNavigatorAddNewItem.Text = "Neu hinzufügen";
+      // 
+      // bindingNavigatorDeleteItem
+      // 
+      this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+      this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+      this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+      this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 26);
+      this.bindingNavigatorDeleteItem.Text = "Löschen";
+      // 
+      // bsRegions
+      // 
+      this.bsRegions.DataMember = "regions";
+      this.bsRegions.DataSource = this.dsMain;
+      // 
+      // dgvRegions
+      // 
+      this.dgvRegions.AutoGenerateColumns = false;
+      this.dgvRegions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dgvRegions.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.regionidDataGridViewTextBoxColumn,
+            this.regionnameDataGridViewTextBoxColumn});
+      this.dgvRegions.DataSource = this.bsRegions;
+      this.dgvRegions.Location = new System.Drawing.Point(44, 61);
+      this.dgvRegions.Name = "dgvRegions";
+      this.dgvRegions.Size = new System.Drawing.Size(502, 285);
+      this.dgvRegions.TabIndex = 1;
+      // 
+      // dsMain
+      // 
+      this.dsMain.DataSetName = "DsCodersBay";
+      this.dsMain.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+      // 
+      // regionidDataGridViewTextBoxColumn
+      // 
+      this.regionidDataGridViewTextBoxColumn.DataPropertyName = "region_id";
+      this.regionidDataGridViewTextBoxColumn.HeaderText = "region_id";
+      this.regionidDataGridViewTextBoxColumn.Name = "regionidDataGridViewTextBoxColumn";
+      this.regionidDataGridViewTextBoxColumn.ReadOnly = true;
+      // 
+      // regionnameDataGridViewTextBoxColumn
+      // 
+      this.regionnameDataGridViewTextBoxColumn.DataPropertyName = "region_name";
+      this.regionnameDataGridViewTextBoxColumn.HeaderText = "region_name";
+      this.regionnameDataGridViewTextBoxColumn.Name = "regionnameDataGridViewTextBoxColumn";
+      // 
+      // FrmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(784, 437);
+      this.Controls.Add(this.dgvRegions);
+      this.Controls.Add(this.navMain);
+      this.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+      this.Name = "FrmMain";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.FrmMain_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.navMain)).EndInit();
+      this.navMain.ResumeLayout(false);
+      this.navMain.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.bsRegions)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dgvRegions)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dsMain)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.BindingNavigator navMain;
+    private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+    private System.Windows.Forms.BindingSource bsRegions;
+    private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+    private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+    private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+    private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+    private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+    private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+    private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+    private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+    private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+    private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+    private System.Windows.Forms.DataGridView dgvRegions;
+    private DAL.DsCodersBay dsMain;
+    private System.Windows.Forms.DataGridViewTextBoxColumn regionidDataGridViewTextBoxColumn;
+    private System.Windows.Forms.DataGridViewTextBoxColumn regionnameDataGridViewTextBoxColumn;
+  }
+}
+
